7ee045d6130f1b6918b97fc633416380bfd8bb65
Jenifer Rodrigues <jen.rodrigues@gmail.com>
2021-05-15 19:57:15 +0100

    Configuring API routes

--- Todo.Domain.Api/Controllers/TodoController.cs
+++ Todo.Domain.Api/Controllers/TodoController.cs
@@ -1,6 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Todo.Domain.Commands;
+using Todo.Domain.Entities;
 using Todo.Domain.Handlers;
+using Todo.Domain.Repositories;
 
 namespace Todo.Api.Controllers
 {
@@ -9,8 +14,119 @@ namespace Todo.Api.Controllers
     public class TodoController : ControllerBase
     {
         [Route("")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAll(
+            [FromServices] ITodoRepository repository)
+        {
+            return repository.GetAll("andrebaltieri");
+        }
+
+        [Route("done")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllDone(
+            [FromServices] ITodoRepository repository
+        )
+        {
+            //var user = User.Claims.FirstOrDefault(x => x.Type == "user_id")?.Value;
+            return repository.GetAllDone("andrebaltieri");
+
+        }
+
+        [Route("undone")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetAllUndone(
+                   [FromServices] ITodoRepository repository)
+        {
+            return repository.GetAllUndone("andrebaltieri");
+        }
+
+        [Route("done/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForToday(
+            [FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod(
+                "andrebaltieri",
+                DateTime.Now.Date,
+                true);
+        }
+
+        [Route("undone/today")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetInactiveForToday(
+                   [FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod(
+                "andrebaltieri",
+                DateTime.Now.Date,
+                false);
+        }
+
+
+        [Route("done/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetDoneForTomorrow(
+                   [FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod(
+                "andrebaltieri",
+                DateTime.Now.AddDays(1),
+                true);
+        }
+
+
+        [Route("undone/tomorrow")]
+        [HttpGet]
+        public IEnumerable<TodoItem> GetUndoneForTomorrow(
+            [FromServices] ITodoRepository repository)
+        {
+            return repository.GetByPeriod(
+                "andrebaltieri",
+                DateTime.Now.AddDays(1),
+                false);
+        }
+
+        [Route("")]
         [HttpPost]
-        public GenericCommandResult Create([FromBody] CreateTodoCommand command, [FromServices] TodoHandler handler)
+        public GenericCommandResult Create(
+                            [FromBody] CreateTodoCommand command,
+                            [FromServices] TodoHandler handler)
+        {
+            command.User = "andrebaltieri";
+            return (GenericCommandResult)handler.Handle(command);
+
+        }
+
+
+
+        [Route("")]
+        [HttpPut]
+        public GenericCommandResult Update(
+            [FromBody] UpdateTitleCommand command,
+            [FromServices] TodoHandler handler)
+        {
+            command.User = "andrebaltieri";
+            return (GenericCommandResult)handler.Handle(command);
+
+        }
+
+
+        [Route("mark-as-done")]
+        [HttpPut]
+        public GenericCommandResult MarkAsDone(
+           [FromBody] MarkTodoAsDoneCommand command,
+           [FromServices] TodoHandler handler)
+        {
+            command.User = "andrebaltieri";
+            return (GenericCommandResult)handler.Handle(command);
+
+        }
+
+        [Route("mark-as-undone")]
+        [HttpPut]
+        public GenericCommandResult MarkAsUndone(
+           [FromBody] MarkTodoAsUndoneCommand command,
+           [FromServices] TodoHandler handler)
         {
             command.User = "andrebaltieri";
             return (GenericCommandResult)handler.Handle(command);
